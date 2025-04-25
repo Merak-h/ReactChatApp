398784a41af59af3882d6083d6b1ad6aa99f52e6
mitsuki <hokuto.pte@gmail.com>
2025-04-25 12:13:35 +0900

    loading画面の追加

--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -1,12 +1,15 @@
-import React, { FC, ReactNode, useEffect } from "react";
+import { FC, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
 import { useLoginUser } from "../hooks/useLoginUser";
+import { Loading } from "../components/pages/Loading";
 
 export const PrivateRoute:FC = () => {
   const { loading, account } = useLoginUser();
+  const [finishFlag, setFinishFlag] = useState<boolean>(false);
 
-  if ( loading ) return <Box>読み込み中...</Box>;
+  if ( !finishFlag ) return (
+    <Loading loadFinish={!loading} callback={setFinishFlag} />
+  );
 
   return account ? <Outlet /> : <Navigate to="/" replace />;
 };
--- src/router/PublicRoute.tsx
+++ src/router/PublicRoute.tsx
@@ -1,11 +1,15 @@
 import { useLoginUser } from "../hooks/useLoginUser";
 import { Navigate, Outlet } from "react-router-dom";
-import { FC } from "react";
+import { FC, useState } from "react";
+import { Loading } from "../components/pages/Loading";
 
 export const PublicRoute:FC = () => {
   const { account, loading } = useLoginUser();
+    const [finishFlag, setFinishFlag] = useState<boolean>(false);
 
-  if (loading) return <div>読み込み中...</div>;
+    if ( !finishFlag ) return (
+      <Loading loadFinish={!loading} callback={setFinishFlag} />
+    );
 
   return account ? <Navigate to="/home" replace /> : <Outlet />;
 };
